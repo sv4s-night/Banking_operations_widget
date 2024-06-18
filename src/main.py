c0916fea23a5550e7712ee7e9adb5bfe6fe4fb7e
SvAS <sv4s.night@gmail.com>
2024-06-18 16:25:07 +0300

    end_of_work

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
-from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card, get_data
 
-
 if __name__ == "__main__":
-    user_card_number = "Maestro 1596837868705199"     #input("Введите номер Вашей карты: ")
-    print(get_mask_card_number(user_card_number))
+    get_user_info = str(input("Введите данные Вашей карты или счета:\n"))
+    print(mask_account_card(get_user_info))
 
-    user_account = "Счет 64686473678894779589"         #input("Введите номер Вашего счета: ")
-    print(get_mask_account(user_account))
-
-    print("==================================================")
-
-    info_card = "Maestro 1596837868705199"
-    print(mask_account_card(info_card))
-
-    info_data = "2018-07-11T02:26:18.671407"
+    info_data = str(input("Введите дату операции:\n"))
     print(get_data(info_data))
--- src/widget.py
+++ src/widget.py
@@ -1,45 +1,26 @@
-"""
-Функция принимает на вход только один аргумент — строку, которая состоит из требуемых частей.
-Это может быть строка типа Visa Platinum 7000 7922 8960 6361,
-или Maestro 7000 7922 8960 6361,
-или Счет 73654108430135874305.
-Разделять строку на 2 аргумента (отдельно имя, отдельно номер) нельзя!
-
-========================================================================================================
-
-Возвращать исходную строку с замаскированным номером карты/счета.
-Для карты и счета используйте разные маскировки.
-
-========================================================================================================
-
-"""
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(mask_info_card):
-    """Функция общей маскировки карты и счета"""
+def mask_account_card(mask_info_card: str) -> str:
+    """Функция маскировки карты и счета"""
 
-    masquerade = mask_info_card.split(' ')
+    masquerade = mask_info_card.split(" ")
 
     for account_or_card in masquerade:
         if account_or_card.isdigit():
-            if len(account_or_card) <= 16:
-                result = masquerade[0] + get_mask_card_number(account_or_card)
-            elif len(account_or_card) > 16:
-                result = masquerade[0] + get_mask_account(account_or_card)
-            else:
-                result = "error"
 
+            if len(account_or_card) == 16:
+                result = " ".join(masquerade[0:-1]) + get_mask_card_number(account_or_card)
+            elif len(account_or_card) > 16:
+                result = " ".join(masquerade[0:-1]) + get_mask_account(account_or_card)
+            else:
+                result = "Вы допустили ошибку при вводе данных счета или карты"
 
     return result
-
-
-
 
 
 def get_data(date_str: str) -> str:
     """Функция преобразования даты"""
 
-    parts = date_str.split('T')[0].split('-')
+    parts = date_str.split("T")[0].split("-")
     return f"{parts[2]}.{parts[1]}.{parts[0]}"
