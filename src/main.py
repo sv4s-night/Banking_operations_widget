bf8ee03c2ea7bf80d6f774e0a3a59a1b31049ba5
SvAS <sv4s.night@gmail.com>
2024-06-18 15:03:44 +0300

    realise def mask_account_card

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from .masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_data
+
 
 if __name__ == "__main__":
-    user_card_number = input("Введите номер Вашей карты: ")
+    user_card_number = "Maestro 1596837868705199"     #input("Введите номер Вашей карты: ")
     print(get_mask_card_number(user_card_number))
 
-    user_account = input("Введите номер Вашего счета: ")
+    user_account = "Счет 64686473678894779589"         #input("Введите номер Вашего счета: ")
     print(get_mask_account(user_account))
+
+    print("==================================================")
+
+    info_card = "Maestro 1596837868705199"
+    print(mask_account_card(info_card))
+
+    info_data = "2018-07-11T02:26:18.671407"
+    print(get_data(info_data))
