134a4691f3aac7f632d94788f672b59ef32ebb85
SvAS <sv4s.night@gmail.com>
2024-06-20 21:31:57 +0300

    add annotation from func

--- src/main.py
+++ src/main.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     # info_data = str(input("Введите дату операции:\n"))
     # print(get_data(info_data))
 
-    dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    dict_list = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
-
-    sorted_dicts = filter_by_state(dict_list, state='CANCELED')
+    sorted_dicts = filter_by_state(dict_list, state="CANCELED")
     print(f"Сортировка по статусу операции:\n{sorted_dicts}")
 
     sorted_data = sort_by_data(dict_list, reverse=True)
--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,13 @@
-def filter_by_state(data_array, state='EXECUTED'):
+from typing import Any
+
+
+def filter_by_state(data_array: Any[str], state: str = "EXECUTED") -> Any[str]:
     """Функция фильтрации в операциях по счетам (исполнено и отказано)"""
 
-    return [d for d in data_array if d.get('state') == state]
+    return [d for d in data_array if d.get("state") == state]
 
 
-def sort_by_data(data_array, reverse=False):
+def sort_by_data(data_array: Any[str], reverse: bool = False) -> Any[str]:
     """Функция сортировки по дате"""
 
-    return sorted(data_array, key=lambda x: x['date'], reverse=reverse)
+    return sorted(data_array, key=lambda x: x["date"], reverse=reverse)
